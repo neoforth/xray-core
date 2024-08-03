2a6f1228a16dee1966971b1eec511faaf2e2034d
Neoforth <60637712+neoforth@users.noreply.github.com>
2024-08-03 01:24:37 -0500

    Update limiter.go

--- app/limiter/limiter.go
+++ app/limiter/limiter.go
@@ -28,77 +28,67 @@ type UserInfo struct {
 }
 
 func New() *Limiter {
-	l := &Limiter{
+	limiter := &Limiter{
 		InboundInfo: new(sync.Map),
 		stopChan:    make(chan struct{}),
 	}
 
-	l.wg.Add(1) // Add a count to the WaitGroup for the cleanup task goroutine
+	limiter.wg.Add(1)
 
-	go l.startCleanupTask(5 * time.Minute)
+	go limiter.startCleanupTask(5 * time.Minute)
 
-	return l
+	return limiter
 }
 
-func (l *Limiter) GetUserBucket(tag string, uid uint32, email string, ipLimit uint32, rateLimit uint64, ip string) (*rate.Limiter, bool, bool) {
-	// Load or create InboundInfo
-	v, _ := l.InboundInfo.LoadOrStore(tag, &InboundInfo{
+func (limiter *Limiter) GetUserBucket(tag string, uid uint32, email string, ipLimit uint32, rateLimit uint64, ip string) (*rate.Limiter, bool, bool) {
+	inboundInfoValue, _ := limiter.InboundInfo.LoadOrStore(tag, &InboundInfo{
 		Tag:           tag,
 		UserInfo:      new(sync.Map),
 		UserOnlineIPs: new(sync.Map),
 		BucketHub:     new(sync.Map),
 	})
-	inboundInfo := v.(*InboundInfo)
+	inboundInfo := inboundInfoValue.(*InboundInfo)
 
-	// Load or create UserInfo
-	v2, _ := inboundInfo.UserInfo.LoadOrStore(email, &UserInfo{
+	userInfoValue, _ := inboundInfo.UserInfo.LoadOrStore(email, &UserInfo{
 		UID:       uid,
 		Email:     email,
 		IPLimit:   ipLimit,
 		RateLimit: rateLimit,
 	})
-	userInfo := v2.(*UserInfo)
+	userInfo := userInfoValue.(*UserInfo)
 
-	// Load or create ipMappings
-	v3, _ := inboundInfo.UserOnlineIPs.LoadOrStore(email, new(sync.Map))
-	ipMappings := v3.(*sync.Map)
+	userOnlineIPsValue, _ := inboundInfo.UserOnlineIPs.LoadOrStore(email, new(sync.Map))
+	userOnlineIPs := userOnlineIPsValue.(*sync.Map)
 
-	// Load or create ipTimestamps
-	v4, ok := ipMappings.LoadOrStore(ip, new(sync.Map))
-	ipTimestamps := v4.(*sync.Map)
+	ipTimestampsValue, ipExists := userOnlineIPs.LoadOrStore(ip, new(sync.Map))
+	ipTimestamps := ipTimestampsValue.(*sync.Map)
 
-	// Store the last seen timestamp for this IP
 	timestamp := time.Now().Unix()
 	ipTimestamps.Store(1, timestamp)
 
-	// Store the first seen timestamp for this IP if first time connecting
-	if !ok {
+	if !ipExists {
 		ipTimestamps.Store(0, timestamp)
 
-		// Manage IP limits
 		if ipLimit > 0 {
 			var ipCount uint32
-			ipMappings.Range(func(_, _ interface{}) bool {
+			userOnlineIPs.Range(func(_, _ interface{}) bool {
 				ipCount++
 				return true
 			})
 
 			if ipCount > ipLimit {
-				ipMappings.Delete(ip)
+				userOnlineIPs.Delete(ip)
 				return nil, false, true
 			}
 		}
 	}
 
-	// Manage rate limits
 	if rateLimit > 0 {
-		if rateLimiter, ok := inboundInfo.BucketHub.Load(email); ok {
+		if rateLimiter, exists := inboundInfo.BucketHub.Load(email); exists {
 			if userInfo.RateLimit == rateLimit {
-				bucket := rateLimiter.(*rate.Limiter)
-				return bucket, true, false
+				return rateLimiter.(*rate.Limiter), true, false
 			}
 			userInfo.RateLimit = rateLimit
-			inboundInfo.BucketHub.Delete(email)
 		}
 		bucket := rate.NewLimiter(rate.Limit(rateLimit), int(rateLimit))
 		inboundInfo.BucketHub.Store(email, bucket)
@@ -108,8 +98,8 @@ func (l *Limiter) GetUserBucket(tag string, uid uint32, email string, ipLimit ui
 	return nil, false, false
 }
 
-func (l *Limiter) startCleanupTask(interval time.Duration) {
-	defer l.wg.Done() // Decrease the count in the WaitGroup when the goroutine finishes
+func (limiter *Limiter) startCleanupTask(interval time.Duration) {
+	defer limiter.wg.Done()
 
 	ticker := time.NewTicker(interval)
 	defer ticker.Stop()
@@ -117,49 +107,42 @@ func (l *Limiter) startCleanupTask(interval time.Duration) {
 	for {
 		select {
 		case <-ticker.C:
-			l.cleanUserOnlineIPs(30 * time.Minute)
-			l.cleanBucketHub()
-		case <-l.stopChan:
+			limiter.cleanUserOnlineIPs(30 * time.Minute)
+			limiter.cleanBucketHub()
+		case <-limiter.stopChan:
 			return
 		}
 	}
 }
 
-func (l *Limiter) cleanUserOnlineIPs(timeout time.Duration) {
-	// Calculate the expiration time
+func (limiter *Limiter) cleanUserOnlineIPs(timeout time.Duration) {
 	expirationTime := time.Now().Add(-timeout).Unix()
 
-	// Iterate over each tag in InboundInfo
-	l.InboundInfo.Range(func(_, value interface{}) bool {
+	limiter.InboundInfo.Range(func(_, value interface{}) bool {
 		inboundInfo := value.(*InboundInfo)
 
-		// Iterate over each email in UserOnlineIPs
 		inboundInfo.UserOnlineIPs.Range(func(key, value interface{}) bool {
 			email := key.(string)
-			ipMappings := value.(*sync.Map)
+			userOnlineIPs := value.(*sync.Map)
 
-			// Iterate over each IP in ipMappings
-			ipMappings.Range(func(key, value interface{}) bool {
+			userOnlineIPs.Range(func(key, value interface{}) bool {
 				ip := key.(string)
 				ipTimestamps := value.(*sync.Map)
 
-				// Delete the IP entry if expired
 				ipTimestamp, _ := ipTimestamps.Load(1)
 				ipLastSeen := ipTimestamp.(int64)
 				if ipLastSeen < expirationTime {
-					ipMappings.Delete(ip)
+					userOnlineIPs.Delete(ip)
 				}
 				return true
 			})
 
-			// Count the number of IPs for this email
 			var ipCount uint32
-			ipMappings.Range(func(_, _ interface{}) bool {
+			userOnlineIPs.Range(func(_, _ interface{}) bool {
 				ipCount++
 				return true
 			})
 
-			// If no IPs under this email, delete the email entry
 			if ipCount == 0 {
 				inboundInfo.UserOnlineIPs.Delete(email)
 			}
@@ -169,24 +152,20 @@ func (l *Limiter) cleanUserOnlineIPs(timeout time.Duration) {
 	})
 }
 
-func (l *Limiter) cleanBucketHub() {
-	// Iterate over each tag in InboundInfo
-	l.InboundInfo.Range(func(_, value interface{}) bool {
+func (limiter *Limiter) cleanBucketHub() {
+	limiter.InboundInfo.Range(func(_, value interface{}) bool {
 		inboundInfo := value.(*InboundInfo)
 
-		// Iterate over each email in UserOnlineIPs
 		inboundInfo.UserOnlineIPs.Range(func(key, value interface{}) bool {
 			email := key.(string)
-			ipMappings := value.(*sync.Map)
+			userOnlineIPs := value.(*sync.Map)
 
-			// Count the number of IPs for this email
 			var ipCount uint32
-			ipMappings.Range(func(_, _ interface{}) bool {
+			userOnlineIPs.Range(func(_, _ interface{}) bool {
 				ipCount++
 				return true
 			})
 
-			// If no IPs under this email, delete the rate limiter
 			if ipCount == 0 {
 				inboundInfo.BucketHub.Delete(email)
 			}
@@ -196,7 +175,7 @@ func (l *Limiter) cleanBucketHub() {
 	})
 }
 
-func (l *Limiter) Stop() {
-	close(l.stopChan)
-	l.wg.Wait() // Wait for all goroutines to finish
+func (limiter *Limiter) Stop() {
+	close(limiter.stopChan)
+	limiter.wg.Wait()
 }
