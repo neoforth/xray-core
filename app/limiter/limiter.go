1054b1379c4f02edefd688cb2a1e12d97154bc3b
Neoforth <60637712+neoforth@users.noreply.github.com>
2024-08-08 20:41:04 -0500

    Update limiter.go

--- app/limiter/limiter.go
+++ app/limiter/limiter.go
@@ -1,8 +1,6 @@
 package limiter
 
 import (
-	"math"
-	"sort"
 	"sync"
 	"time"
 
@@ -16,25 +14,17 @@ type Limiter struct {
 }
 
 type InboundInfo struct {
-	Tag           string
-	UserInfo      *sync.Map // Key: Email, Value: *UserInfo
-	UserOnlineIPs *sync.Map // Key: Email, Value: *{Key: IP, Value: *IPStatus}
-	BucketHub     *sync.Map // Key: Email, Value: *rate.Limiter
+	Tag          string
+	UserInfo     *sync.Map // Key: Email, Value: *UserInfo
+	OnlineIPs    *sync.Map // Key: Email, Value: *{Key: IP, Value: *IPStatus}
+	TokenBuckets *sync.Map // Key: Email, Value: *rate.Limiter
 }
 
 type UserInfo struct {
-	UID       uint32
+	ID        uint32
 	Email     string
 	IPLimit   uint32
 	RateLimit uint64
-}
-
-type IPStatus struct {
-	AccessCount   uint64
-	FirstSeen     int64
-	LastSeen      int64
-	ActivityScore float64
-	mu            sync.RWMutex
 }
 
 func New() *Limiter {
@@ -45,51 +35,31 @@ func New() *Limiter {
 
 	limiter.wg.Add(1)
 
-	go limiter.startCleanupTask(5 * time.Minute)
+	go limiter.startCleanupTask(1 * time.Minute)
 
 	return limiter
 }
 
-func (limiter *Limiter) GetUserBucket(tag string, uid uint32, email string, ipLimit uint32, rateLimit uint64, ip string) (*rate.Limiter, bool, bool) {
+func (limiter *Limiter) GetUserBucket(tag string, id uint32, email string, ipLimit uint32, rateLimit uint64, ip string) (*rate.Limiter, bool, bool) {
 	inboundInfoValue, _ := limiter.InboundInfo.LoadOrStore(tag, &InboundInfo{
-		Tag:           tag,
-		UserInfo:      new(sync.Map),
-		UserOnlineIPs: new(sync.Map),
-		BucketHub:     new(sync.Map),
+		Tag:          tag,
+		UserInfo:     new(sync.Map),
+		OnlineIPs:    new(sync.Map),
+		TokenBuckets: new(sync.Map),
 	})
 	inboundInfo := inboundInfoValue.(*InboundInfo)
 
-	userInfoValue, _ := inboundInfo.UserInfo.LoadOrStore(email, &UserInfo{
-		UID:       uid,
-		Email:     email,
-		IPLimit:   ipLimit,
-		RateLimit: rateLimit,
-	})
-	userInfo := userInfoValue.(*UserInfo)
-
-	ipMappingsValue, _ := inboundInfo.UserOnlineIPs.LoadOrStore(email, new(sync.Map))
+	ipMappingsValue, _ := inboundInfo.OnlineIPs.LoadOrStore(email, new(sync.Map))
 	ipMappings := ipMappingsValue.(*sync.Map)
 
-	ipStatusValue, ipExists := ipMappings.LoadOrStore(ip, &IPStatus{
-		AccessCount:   0,
-		FirstSeen:     0,
-		LastSeen:      0,
-		ActivityScore: 0,
-	})
-	ipStatus := ipStatusValue.(*IPStatus)
+	_, ipExists := ipMappings.LoadOrStore(ip, new(int64))
 
+	// IP access
 	timestamp := time.Now().Unix()
+	ipMappings.Store(ip, timestamp)
 
-	ipStatus.mu.Lock()
-	ipStatus.AccessCount++
-	ipStatus.LastSeen = timestamp
-	ipStatus.mu.Unlock()
-
+	// IP limit
 	if !ipExists {
-		ipStatus.mu.Lock()
-		ipStatus.FirstSeen = timestamp
-		ipStatus.mu.Unlock()
-
 		if ipLimit > 0 {
 			var ipCount uint32
 
@@ -97,10 +67,6 @@ func (limiter *Limiter) GetUserBucket(tag string, uid uint32, email string, ipLi
 				ipCount++
 				return true
 			})
-
-			if userInfo.IPLimit != ipLimit {
-				userInfo.IPLimit = ipLimit
-			}
 
 			if ipCount > ipLimit {
 				ipMappings.Delete(ip)
@@ -110,16 +76,14 @@ func (limiter *Limiter) GetUserBucket(tag string, uid uint32, email string, ipLi
 		}
 	}
 
+	// Rate limit
 	if rateLimit > 0 {
-		if rateLimiter, emailExists := inboundInfo.BucketHub.Load(email); emailExists {
-			if userInfo.RateLimit == rateLimit {
-				return rateLimiter.(*rate.Limiter), true, false
-			}
-			userInfo.RateLimit = rateLimit
+		if rateLimiter, emailExists := inboundInfo.TokenBuckets.Load(email); emailExists {
+			return rateLimiter.(*rate.Limiter), true, false
 		}
 
 		bucket := rate.NewLimiter(rate.Limit(rateLimit), int(rateLimit))
-		inboundInfo.BucketHub.Store(email, bucket)
+		inboundInfo.TokenBuckets.Store(email, bucket)
 
 		return bucket, true, false
 	}
@@ -136,57 +100,23 @@ func (limiter *Limiter) startCleanupTask(interval time.Duration) {
 	for {
 		select {
 		case <-ticker.C:
-			limiter.cleanUserOnlineIPs(5 * time.Minute)
-			limiter.cleanBucketHub()
+			limiter.cleanOnlineIPs(5 * time.Minute)
+			limiter.cleanTokenBuckets()
 		case <-limiter.stopChan:
 			return
 		}
 	}
 }
 
-func (limiter *Limiter) cleanUserOnlineIPs(timeout time.Duration) {
-	now := time.Now().Unix()
-	windowSize := int64(timeout.Seconds())
+func (limiter *Limiter) cleanOnlineIPs(timeout time.Duration) {
+	expirationTime := time.Now().Add(-timeout).Unix()
 
 	limiter.InboundInfo.Range(func(_, value interface{}) bool {
 		inboundInfo := value.(*InboundInfo)
 
-		var allScores []float64
-
-		inboundInfo.UserOnlineIPs.Range(func(_, value interface{}) bool {
-			ipMappings := value.(*sync.Map)
-
-			ipMappings.Range(func(_, value interface{}) bool {
-				ipStatus := value.(*IPStatus)
-
-				ipStatus.mu.RLock()
-				accessCount := ipStatus.AccessCount
-				timeSinceFirstSeen := now - ipStatus.FirstSeen
-				timeSinceLastSeen := now - ipStatus.LastSeen
-				ipStatus.mu.RUnlock()
-
-				activityScore := float64(accessCount) / float64(timeSinceFirstSeen)
-				decayFactor := math.Exp(-float64(timeSinceLastSeen) / float64(windowSize))
-
-				ipActivityScore := activityScore * decayFactor
-
-				ipStatus.mu.Lock()
-				ipStatus.ActivityScore = ipActivityScore
-				ipStatus.mu.Unlock()
-
-				allScores = append(allScores, ipActivityScore)
-
-				return true
-			})
-
-			return true
-		})
-
-		threshold := calculateThreshold(allScores)
-
 		var emailsToDelete []string
 
-		inboundInfo.UserOnlineIPs.Range(func(key, value interface{}) bool {
+		inboundInfo.OnlineIPs.Range(func(key, value interface{}) bool {
 			email := key.(string)
 			ipMappings := value.(*sync.Map)
 
@@ -194,14 +124,9 @@ func (limiter *Limiter) cleanUserOnlineIPs(timeout time.Duration) {
 
 			ipMappings.Range(func(key, value interface{}) bool {
 				ip := key.(string)
-				ipStatus := value.(*IPStatus)
+				ipTimestamp := value.(int64)
 
-				ipStatus.mu.RLock()
-				timeSinceLastSeen := now - ipStatus.LastSeen
-				ipActivityScore := ipStatus.ActivityScore
-				ipStatus.mu.RUnlock()
-
-				if timeSinceLastSeen > windowSize && ipActivityScore < threshold {
+				if ipTimestamp < expirationTime {
 					ipsToDelete = append(ipsToDelete, ip)
 				}
 
@@ -227,20 +152,20 @@ func (limiter *Limiter) cleanUserOnlineIPs(timeout time.Duration) {
 		})
 
 		for _, email := range emailsToDelete {
-			inboundInfo.UserOnlineIPs.Delete(email)
+			inboundInfo.OnlineIPs.Delete(email)
 		}
 
 		return true
 	})
 }
 
-func (limiter *Limiter) cleanBucketHub() {
+func (limiter *Limiter) cleanTokenBuckets() {
 	limiter.InboundInfo.Range(func(_, value interface{}) bool {
 		inboundInfo := value.(*InboundInfo)
 
 		var emailsToDelete []string
 
-		inboundInfo.UserOnlineIPs.Range(func(key, value interface{}) bool {
+		inboundInfo.OnlineIPs.Range(func(key, value interface{}) bool {
 			email := key.(string)
 			ipMappings := value.(*sync.Map)
 
@@ -259,7 +184,7 @@ func (limiter *Limiter) cleanBucketHub() {
 		})
 
 		for _, email := range emailsToDelete {
-			inboundInfo.BucketHub.Delete(email)
+			inboundInfo.TokenBuckets.Delete(email)
 		}
 
 		return true
@@ -269,26 +194,4 @@ func (limiter *Limiter) cleanBucketHub() {
 func (limiter *Limiter) Stop() {
 	close(limiter.stopChan)
 	limiter.wg.Wait()
-}
-
-func calculateThreshold(scores []float64) float64 {
-	if len(scores) == 0 {
-		return 0
-	}
-
-	sort.Float64s(scores)
-
-	var median float64
-	num := len(scores)
-	if num%2 == 0 {
-		median = (scores[num/2-1] + scores[num/2]) / 2
-	} else {
-		median = scores[num/2]
-	}
-
-	if median < 0 {
-		return 0
-	}
-
-	return median
 }
