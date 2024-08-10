a60c658860915cc5fe3ffd51541df5f893a220fb
Neoforth <60637712+neoforth@users.noreply.github.com>
2024-08-09 19:42:26 -0500

    Update limiter.go

--- app/limiter/limiter.go
+++ app/limiter/limiter.go
@@ -15,84 +15,25 @@ type Limiter struct {
 
 type InboundInfo struct {
 	Tag          string
-	UserInfo     *sync.Map // Key: Email, Value: *UserInfo
-	OnlineIPs    *sync.Map // Key: Email, Value: *{Key: IP, Value: *IPStatus}
-	TokenBuckets *sync.Map // Key: Email, Value: *rate.Limiter
-}
-
-type UserInfo struct {
-	ID        uint32
-	Email     string
-	IPLimit   uint32
-	RateLimit uint64
+	UserIPs      *sync.Map // Key: Email, Value: *{Key: IP, Value: Timestamp}
+	RateLimiters *sync.Map // Key: Email, Value: *rate.Limiter
 }
 
 func New() *Limiter {
-	limiter := &Limiter{
+	l := &Limiter{
 		InboundInfo: new(sync.Map),
 		stopChan:    make(chan struct{}),
 	}
 
-	limiter.wg.Add(1)
+	l.wg.Add(1)
 
-	go limiter.startCleanupTask(1 * time.Minute)
+	go l.Start(1 * time.Minute)
 
-	return limiter
+	return l
 }
 
-func (limiter *Limiter) GetUserBucket(tag string, id uint32, email string, ipLimit uint32, rateLimit uint64, ip string) (*rate.Limiter, bool, bool) {
-	inboundInfoValue, _ := limiter.InboundInfo.LoadOrStore(tag, &InboundInfo{
-		Tag:          tag,
-		UserInfo:     new(sync.Map),
-		OnlineIPs:    new(sync.Map),
-		TokenBuckets: new(sync.Map),
-	})
-	inboundInfo := inboundInfoValue.(*InboundInfo)
-
-	ipMappingsValue, _ := inboundInfo.OnlineIPs.LoadOrStore(email, new(sync.Map))
-	ipMappings := ipMappingsValue.(*sync.Map)
-
-	_, ipExists := ipMappings.LoadOrStore(ip, new(int64))
-
-	// IP access
-	timestamp := time.Now().Unix()
-	ipMappings.Store(ip, timestamp)
-
-	// IP limit
-	if !ipExists {
-		if ipLimit > 0 {
-			var ipCount uint32
-
-			ipMappings.Range(func(_, _ interface{}) bool {
-				ipCount++
-				return true
-			})
-
-			if ipCount > ipLimit {
-				ipMappings.Delete(ip)
-
-				return nil, false, true
-			}
-		}
-	}
-
-	// Rate limit
-	if rateLimit > 0 {
-		if rateLimiter, emailExists := inboundInfo.TokenBuckets.Load(email); emailExists {
-			return rateLimiter.(*rate.Limiter), true, false
-		}
-
-		bucket := rate.NewLimiter(rate.Limit(rateLimit), int(rateLimit))
-		inboundInfo.TokenBuckets.Store(email, bucket)
-
-		return bucket, true, false
-	}
-
-	return nil, false, false
-}
-
-func (limiter *Limiter) startCleanupTask(interval time.Duration) {
-	defer limiter.wg.Done()
+func (l *Limiter) Start(interval time.Duration) {
+	defer l.wg.Done()
 
 	ticker := time.NewTicker(interval)
 	defer ticker.Stop()
@@ -100,29 +41,81 @@ func (limiter *Limiter) startCleanupTask(interval time.Duration) {
 	for {
 		select {
 		case <-ticker.C:
-			limiter.cleanOnlineIPs(5 * time.Minute)
-			limiter.cleanTokenBuckets()
-		case <-limiter.stopChan:
+			l.Clean(5 * time.Minute)
+		case <-l.stopChan:
 			return
 		}
 	}
 }
 
-func (limiter *Limiter) cleanOnlineIPs(timeout time.Duration) {
+func (l *Limiter) Stop() {
+	close(l.stopChan)
+	l.wg.Wait()
+}
+
+func (l *Limiter) Get(tag string, email string, ipLimit uint32, rateLimit uint64, ip string) (*rate.Limiter, bool, bool) {
+	inboundInfoValue, _ := l.InboundInfo.LoadOrStore(tag, &InboundInfo{
+		Tag:          tag,
+		UserIPs:      new(sync.Map),
+		RateLimiters: new(sync.Map),
+	})
+	inboundInfo := inboundInfoValue.(*InboundInfo)
+
+	ipMapsValue, _ := inboundInfo.UserIPs.LoadOrStore(email, new(sync.Map))
+	ipMaps := ipMapsValue.(*sync.Map)
+
+	_, ipExists := ipMaps.LoadOrStore(ip, new(int64))
+
+	// Record IP access
+	timestamp := time.Now().Unix()
+	ipMaps.Store(ip, timestamp)
+
+	// Enforce IP limit
+	if !ipExists && ipLimit > 0 {
+		var ipCount uint32
+
+		ipMaps.Range(func(_, _ interface{}) bool {
+			ipCount++
+			return true
+		})
+
+		if ipCount > ipLimit {
+			ipMaps.Delete(ip)
+
+			return nil, false, true
+		}
+	}
+
+	// Enforce rate limit
+	if rateLimit > 0 {
+		if rateLimiter, emailExists := inboundInfo.RateLimiters.Load(email); emailExists {
+			return rateLimiter.(*rate.Limiter), true, false
+		}
+
+		bucket := rate.NewLimiter(rate.Limit(rateLimit), int(rateLimit))
+		inboundInfo.RateLimiters.Store(email, bucket)
+
+		return bucket, true, false
+	}
+
+	return nil, false, false
+}
+
+func (l *Limiter) Clean(timeout time.Duration) {
 	expirationTime := time.Now().Add(-timeout).Unix()
 
-	limiter.InboundInfo.Range(func(_, value interface{}) bool {
+	l.InboundInfo.Range(func(_, value interface{}) bool {
 		inboundInfo := value.(*InboundInfo)
 
 		var emailsToDelete []string
 
-		inboundInfo.OnlineIPs.Range(func(key, value interface{}) bool {
+		inboundInfo.UserIPs.Range(func(key, value interface{}) bool {
 			email := key.(string)
-			ipMappings := value.(*sync.Map)
+			ipMaps := value.(*sync.Map)
 
 			var ipsToDelete []string
 
-			ipMappings.Range(func(key, value interface{}) bool {
+			ipMaps.Range(func(key, value interface{}) bool {
 				ip := key.(string)
 				ipTimestamp := value.(int64)
 
@@ -134,12 +127,12 @@ func (limiter *Limiter) cleanOnlineIPs(timeout time.Duration) {
 			})
 
 			for _, ip := range ipsToDelete {
-				ipMappings.Delete(ip)
+				ipMaps.Delete(ip)
 			}
 
 			var ipCount uint32
 
-			ipMappings.Range(func(_, _ interface{}) bool {
+			ipMaps.Range(func(_, _ interface{}) bool {
 				ipCount++
 				return true
 			})
@@ -152,46 +145,10 @@ func (limiter *Limiter) cleanOnlineIPs(timeout time.Duration) {
 		})
 
 		for _, email := range emailsToDelete {
-			inboundInfo.OnlineIPs.Delete(email)
+			inboundInfo.UserIPs.Delete(email)
+			inboundInfo.RateLimiters.Delete(email)
 		}
 
 		return true
 	})
-}
-
-func (limiter *Limiter) cleanTokenBuckets() {
-	limiter.InboundInfo.Range(func(_, value interface{}) bool {
-		inboundInfo := value.(*InboundInfo)
-
-		var emailsToDelete []string
-
-		inboundInfo.OnlineIPs.Range(func(key, value interface{}) bool {
-			email := key.(string)
-			ipMappings := value.(*sync.Map)
-
-			var ipCount uint32
-
-			ipMappings.Range(func(_, _ interface{}) bool {
-				ipCount++
-				return true
-			})
-
-			if ipCount == 0 {
-				emailsToDelete = append(emailsToDelete, email)
-			}
-
-			return true
-		})
-
-		for _, email := range emailsToDelete {
-			inboundInfo.TokenBuckets.Delete(email)
-		}
-
-		return true
-	})
-}
-
-func (limiter *Limiter) Stop() {
-	close(limiter.stopChan)
-	limiter.wg.Wait()
 }
